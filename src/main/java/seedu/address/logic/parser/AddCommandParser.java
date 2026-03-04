fd4d4d949d930d2ffaedd141b06e06b10c8217b8
Jolene <jolenechong7@gmail.com>
2026-03-04 21:06:17 +0800

    add error handling for missing required fields

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -1,14 +1,20 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.Messages.MESSAGE_MISSING_FIELD_FORMAT;
+import static seedu.address.logic.Messages.MESSAGE_MISSING_PREFIX;
+import static seedu.address.logic.Messages.MESSAGE_NON_PREFIX_BEFORE_PREFIX;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.ParserConstants.COMMA_SEPARATOR;
+import static seedu.address.logic.parser.ParserConstants.FIELD_ADDRESS;
+import static seedu.address.logic.parser.ParserConstants.FIELD_EMAIL;
+import static seedu.address.logic.parser.ParserConstants.FIELD_NAME;
+import static seedu.address.logic.parser.ParserConstants.FIELD_PHONE;
 
 import java.util.Set;
-import java.util.stream.Stream;
 
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -24,6 +30,9 @@ import seedu.address.model.tag.Tag;
  */
 public class AddCommandParser implements Parser<AddCommand> {
 
+
+    private record RequiredField(Prefix prefix, String name) {}
+
     /**
      * Parses the given {@code String} of arguments in the context of the AddCommand
      * and returns an AddCommand object for execution.
@@ -33,9 +42,15 @@ public class AddCommandParser implements Parser<AddCommand> {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
 
-        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)
-                || !argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        requirePrefixes(argMultimap,
+                new RequiredField(PREFIX_NAME, FIELD_NAME),
+                new RequiredField(PREFIX_PHONE, FIELD_PHONE),
+                new RequiredField(PREFIX_EMAIL, FIELD_EMAIL),
+                new RequiredField(PREFIX_ADDRESS, FIELD_ADDRESS)
+        );
+
+        if (!argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(MESSAGE_NON_PREFIX_BEFORE_PREFIX);
         }
 
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS);
@@ -51,11 +66,25 @@ public class AddCommandParser implements Parser<AddCommand> {
     }
 
     /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * Throws a ParseException if any of the prefixes are missing in the given
      * {@code ArgumentMultimap}.
      */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    private static void requirePrefixes(ArgumentMultimap map,
+                                        RequiredField... requiredFields) throws ParseException {
+        StringBuilder missing = new StringBuilder();
+
+        for (RequiredField field : requiredFields) {
+            if (map.getValue(field.prefix).isEmpty()) {
+                if (!missing.isEmpty()) {
+                    missing.append(COMMA_SEPARATOR);
+                }
+                missing.append(String.format(MESSAGE_MISSING_FIELD_FORMAT, field.prefix, field.name));
+            }
+        }
+        if (!missing.isEmpty()) {
+            throw new ParseException(MESSAGE_MISSING_PREFIX + missing);
+        }
+
     }
 
 }
