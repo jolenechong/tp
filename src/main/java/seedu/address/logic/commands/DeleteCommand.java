e074950386838924fc3ecfc73cdc95ff42acb5b3
Ming Xuan <66899567+icyyolo@users.noreply.github.com>
2026-03-07 13:38:30 +0800

    Make error description more descriptive

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -23,9 +23,9 @@ public class DeleteCommand extends Command {
             + "Parameters: INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1";
 
-    public static final String VALID_DELETE_RANGE = "\nThe valid delete range is from 1 to ";
+    public static final String VALID_CONTACT_DELETE_RANGE = "\nThe valid delete range is from 1 to ";
 
-    public static final String DATA_IS_EMPTY = "\nThere is no data to delete. Consider adding some data.";
+    public static final String CONTACT_IS_EMPTY = "\nThere is no contact to delete. Consider adding some contacts.";
 
     public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Person: %1$s";
 
@@ -44,10 +44,10 @@ public class DeleteCommand extends Command {
             String exceptionMessage;
             if (lastShownList.isEmpty()) {
                 exceptionMessage = Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX
-                        + DATA_IS_EMPTY;
+                        + CONTACT_IS_EMPTY;
             } else {
                 exceptionMessage = Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX
-                        + VALID_DELETE_RANGE + lastShownList.size();
+                        + VALID_CONTACT_DELETE_RANGE + lastShownList.size();
             }
             throw new CommandException(exceptionMessage);
         }
--- src/test/java/seedu/address/logic/LogicManagerTest.java
+++ src/test/java/seedu/address/logic/LogicManagerTest.java
@@ -7,7 +7,7 @@ import static seedu.address.logic.commands.CommandTestUtil.ADDRESS_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.EMAIL_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.NAME_DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.PHONE_DESC_AMY;
-import static seedu.address.logic.commands.DeleteCommand.DATA_IS_EMPTY;
+import static seedu.address.logic.commands.DeleteCommand.CONTACT_IS_EMPTY;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.AMY;
 
@@ -38,7 +38,7 @@ public class LogicManagerTest {
     private static final IOException DUMMY_IO_EXCEPTION = new IOException("dummy IO exception");
     private static final IOException DUMMY_AD_EXCEPTION = new AccessDeniedException("dummy access denied exception");
 
-    private static final String DELETE_COMMAND_EXCEPTION = MESSAGE_INVALID_PERSON_DISPLAYED_INDEX + DATA_IS_EMPTY;
+    private static final String DELETE_COMMAND_EXCEPTION = MESSAGE_INVALID_PERSON_DISPLAYED_INDEX + CONTACT_IS_EMPTY;
 
     @TempDir
     public Path temporaryFolder;
