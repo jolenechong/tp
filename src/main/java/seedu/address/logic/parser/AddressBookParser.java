4f65e852456c8b462a0f06af25b51421a96c9912
Shermaine <pehshermaine@gmail.com>
2026-03-13 10:17:00 +0800

    improve error handling

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -39,6 +39,8 @@ public class AddressBookParser {
      */
     private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
     private static final Logger logger = LogsCenter.getLogger(AddressBookParser.class);
+    private static final String INVALID_COMMAND_WORD =
+        "Unknown command: listproducts. Did you mean 'listproduct'?";
 
     /**
      * Parses user input into command for execution.
@@ -110,7 +112,7 @@ public class AddressBookParser {
                 return new ListProductsCommand();
 
             case "listproducts":
-                throw new ParseException("Unknown command: listproducts. Did you mean 'listproduct'?");
+                throw new ParseException(INVALID_COMMAND_WORD);
 
             case ArchiveProductCommand.COMMAND_WORD:
                 return new ArchiveProductCommandParser().parse(arguments);
