56e2a6b02acd6ffa49b7416f28ed580bcb26d8a8
Le Hua <l3hu4l1@gmail.com>
2026-04-13 21:40:07 +0800

    Update JsonUtil.java

--- src/main/java/seedu/address/commons/util/JsonUtil.java
+++ src/main/java/seedu/address/commons/util/JsonUtil.java
@@ -70,7 +70,6 @@ public class JsonUtil {
         try {
             jsonFile = deserializeObjectFromJsonFile(filePath, classOfObjectToDeserialize);
         } catch (IOException e) {
-            logger.warning("Error reading from jsonFile file " + filePath + ": " + e);
             throw new DataLoadingException(e);
         }
 
